5b6f64b857959ded9a7705840b718f8fd7a19a8e
Stefano Rasi <rasi.stefano@gmail.com>
2026-01-02 09:25:12 +0100

    Clean up demo text

--- views/app.rb
+++ views/app.rb
@@ -3,8 +3,15 @@ require 'lib/Vi/vi.rb'
 require 'lib/View/window'
 require 'lib/View/document'
 
+TEXT = [
+    "line 1",
+    "line 2",
+    "line 3",
+    "line 4"
+].join("\n")
+
 Window.addEventListener('load') do
-    vi = Vi.new("line 1\nline 2\nline 3\nline 4")
+    vi = Vi.new(TEXT)
 
     Document.body.appendChild(vi.element)
 
